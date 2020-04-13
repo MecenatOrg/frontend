e727c9cc88d9c2da3f672730063afed14a986fdf
Oleg Dolenchuk <olegdolenchuk@gmail.com>
2020-04-13 17:53:48 +0300

    Remove Line Height

--- src/app/App/styles.ts
+++ src/app/App/styles.ts
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 export const AppWrapper = styled.div`
   font-family: 'Montserrat', sans-serif;
   font-size: 16px;
-  line-height: 26px;
   font-weight: 500;
   button,
   input,
