a92564f2fa4d831e1bf2562d07bb077b33894e03
Alex Zavhorodnii <a.zavhorodnii@fezr.com>
2020-04-15 11:47:38 +0300

    Fixed responsive for card images

--- src/shared/components/Partner/styles.ts
+++ src/shared/components/Partner/styles.ts
@@ -6,17 +6,18 @@ export const PartnerWrapper = styled.div`
   justify-content: center;
   width: 100%;
   height: 140px;
+  padding: 35px 60px;
   background: #ffffff;
+  margin-top: 14px;
+  @media (max-width: 576px) {
+    padding: 22px 37px;
+  }
   &:hover {
     box-shadow: 0px 20px 30px rgba(233, 229, 221, 0.6);
   }
 `;
 export const ImageElement = styled.img`
   display: block;
-  width: 140px;
+  width: 100%;
   height: auto;
-
-  @media (max-width: 576px) {
-    width: 88.45px;
-  }
 `;
