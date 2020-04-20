e4bbb688ee2ac6171c2f6fd38a570ab386968a14
Kateryna Vitruk <katherina.vitruk@gmail.com>
2020-04-20 19:28:30 +0300

    feat: update favicon and page title

--- src/app/pages/HomePage/component.tsx
+++ src/app/pages/HomePage/component.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 
 const HomePage: React.FC = () => {
-  return <>Home Page!</>;
+  return (
+    <>
+      <Helmet>
+        <title>Меценат - Головна сторінка</title>
+      </Helmet>
+      {/*Page Content goes here*/}
+    </>
+  );
 };
 
 export default HomePage;
