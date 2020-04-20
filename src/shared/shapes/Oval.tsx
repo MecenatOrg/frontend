04970f01b716fa61a7d03eed7c1e6308ecd47a37
IhorP <eldhim@gmail.com>
2020-04-20 11:17:33 +0300

    [IhorP] add shapes components

---
+++ src/shared/shapes/Oval.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Oval: React.FC = () => (
+  <svg width="193" height="193" viewBox="0 0 193 193" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M96.4054 192.811C149.649 192.811 192.811 149.649 192.811 96.4054C192.811 43.1622 149.649 0 96.4054 0C43.1622 0 0 43.1622 0 96.4054C0 149.649 43.1622 192.811 96.4054 192.811Z"
+      fill="#D2BEE5"
+    />
+  </svg>
+);
+export default Oval;
---
+++ src/shared/shapes/RectangleOvalToRight.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const RectangleOvalToRight: React.FC = () => (
+  <svg width="170" height="194" viewBox="0 0 170 194" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M0 0H170V194H0C0 194 11.1027 119.828 11.1027 81.573C11.1027 43.3184 0 0 0 0Z"
+      fill="#FFD167"
+    />
+  </svg>
+);
+export default RectangleOvalToRight;
