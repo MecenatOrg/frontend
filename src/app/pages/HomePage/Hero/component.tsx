f7bb3301861326f1b00134107f8a7b0eec807e71
Kateryna Vitruk <katherina.vitruk@gmail.com>
2020-05-03 18:33:02 +0300

    feat: add and configure i18n, add translation file

--- src/app/pages/HomePage/Hero/component.tsx
+++ src/app/pages/HomePage/Hero/component.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
+
 import { H1 } from 'shared/Typography';
 import { BigBoxIcon } from 'shared/icons';
 import { Button } from 'shared/components';
@@ -6,16 +8,20 @@ import { Button } from 'shared/components';
 import { BigBoxIconWrapper, HeroTextWrapper, HeroWrapper } from './styles';
 import { ButtonVariant } from 'shared/components/Button/component';
 
-const Hero: React.FC = () => (
-  <HeroWrapper>
-    <HeroTextWrapper>
-      <H1>Соціальні та благодійні ініціативи України</H1>
-      <p>Ви можете знайти проект, який підходить вам ідеально, і дізнатися, як ваша компанія може підтримати його.</p>
-      <Button variant={ButtonVariant.PRIMARY} title="Хочу допомогти" />
-    </HeroTextWrapper>
-    <BigBoxIconWrapper>
-      <BigBoxIcon />
-    </BigBoxIconWrapper>
-  </HeroWrapper>
-);
+const Hero: React.FC = () => {
+  const { t } = useTranslation();
+
+  return (
+    <HeroWrapper>
+      <HeroTextWrapper>
+        <H1>{t('hero.HEADING')}</H1>
+        <p>{t('hero.DESCRIPTION')}</p>
+        <Button variant={ButtonVariant.PRIMARY} title={t('WANT_TO_HELP')} />
+      </HeroTextWrapper>
+      <BigBoxIconWrapper>
+        <BigBoxIcon />
+      </BigBoxIconWrapper>
+    </HeroWrapper>
+  );
+};
 export default Hero;
--- src/shared/components/Footer/component.tsx
+++ src/shared/components/Footer/component.tsx
@@ -1,23 +1,29 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import { useTranslation } from 'react-i18next';
+
 import { LogoWhiteIcon, BoxIcon } from 'shared/icons';
 import { FooterWrapper, LogoWrapper, CopyrightWrapper, Copyright } from './styles';
 
 import FooterList from './FooterList';
 
-const Footer: React.FC = () => (
-  <FooterWrapper>
-    <Container>
-      <LogoWrapper>
-        <LogoWhiteIcon />
-      </LogoWrapper>
-      <FooterList />
-      <CopyrightWrapper>
-        <Copyright>(c) 2019. МЕЦЕНАТ</Copyright>
-        <BoxIcon />
-      </CopyrightWrapper>
-    </Container>
-  </FooterWrapper>
-);
+const Footer: React.FC = () => {
+  const { t } = useTranslation();
+
+  return (
+    <FooterWrapper>
+      <Container>
+        <LogoWrapper>
+          <LogoWhiteIcon />
+        </LogoWrapper>
+        <FooterList />
+        <CopyrightWrapper>
+          <Copyright>{t('footer.COPYRIGHT')}</Copyright>
+          <BoxIcon />
+        </CopyrightWrapper>
+      </Container>
+    </FooterWrapper>
+  );
+};
 
 export default Footer;
